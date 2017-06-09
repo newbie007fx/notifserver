a369213b1a023e4ad910b151ae6cadfce2bf67fb
newbie007fx <newbie007fx@gmail.com>
2017-06-10 01:18:28 +0700

    print port

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var server     = require('http').createServer(),
     connectionsArray    = [],
     soket;
 
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
+var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
+
 // Logger config
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, { colorize: true, timestamp: true });
-logger.info('SocketIO > listening on port ' + port);
+logger.info('SocketIO > listening on address '+address+' port '+port);
 
 
 io.sockets.on( 'connection', function ( socket ) {
